3c81cf80357a13ef78243106940f5e87c8241282
Tayfun Erbilen <tayfunerbilen@positive.com.tr>
2022-05-19 18:52:14 +0300

    ortam değişkenleri

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from "./logo.svg"
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      // analytici bilgilendir
+    }
+  }, [])
+
   return (
     <div className="App">
-      <img src="/logo192.png" alt=""/>
-      <img src={logo} alt=""/>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src="/logo192.png" alt=""/>
+          <img src={logo} alt=""/>
+        </>
+      )}
     </div>
   );
 }
